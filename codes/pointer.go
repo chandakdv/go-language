f8cde4214521a5f2ca543fb711188bcb93b16381
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:17:10 +0530

    Add files via upload

---
+++ codes/pointer.go
@@ -0,0 +1,42 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// *p is a pointer to an integer
+// p=&a this means p is pointing to a
+// func main() {
+
+// //integer = 23   output ==.\pointer.go:8:2: undefined: integer.   \pointer.go:9:23: undefined: integer
+// integer := 23   // output =int*int
+// fmt.Printf("%T%T\n", integer, &integer)
+
+// number, floatingNumber := 238, 1234.57467598
+// fmt.Printf("Default:%f\n", floatingNumber)
+// fmt.Printf("2f:%2f\n", floatingNumber)
+// fmt.Printf(".4f:%4f\n", floatingNumber)
+// fmt.Printf(".5f:%5f\n", floatingNumber)
+// fmt.Printf("Scirntific:%e\n", floatingNumber)
+// fmt.Printf("Decimal:%d\n", number)
+// fmt.Printf("Binary:%b\n", number)
+// fmt.Printf("Octal:%o\n", number)
+// fmt.Printf("HeaxaDecimal:%X\n", number)
+
+//text := "Rajesh"
+
+//fmt.Printf("%*s\n", 40, text) //output =                                  Rajesh
+//fmt.Printf("%040s", text)     ///output=0000000000000000000000000000000000Rajesh
+
+// fmt.Printf("%*s\n", 40, text) //output =                                   Rajesh
+//fmt.Printf("%040s", text) // output = 0000000000000000000000000000000000Rajesh
+
+// var val byte = 'A'
+// fmt.Printf("character value:%c", val)
+// fmt.Printf("\nASCII value:%d", val)
+
+// 	Ch := 'A'
+// 	St := 'B'
+// 	fmt.Printf("%T %T", Ch, St)
+
+// }
