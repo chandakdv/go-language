0f77f33c8e3221fabc0f19bab4f7374b1746c516
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:13:33 +0530

    Add files via upload

---
+++ codes/write_string.go
@@ -0,0 +1,49 @@
+package main
+
+// import (
+// 	"fmt"
+// 	//"math"
+// 	//"io"
+// 	//"os"
+// )
+
+// func main() {
+
+// var dd int = 20
+// var mm int = 02
+// var yy int = 2023
+// var str string
+// str = fmt.Sprintf("%02d-%02d-%04d", dd, mm, yy)
+// io.WriteString(os.Stdout, str)
+
+// var str string
+// var str1 string
+// var rollno int
+// fmt.Printf("enter name:\n")
+// //fmt.Scan(&str)
+// fmt.Printf("enter branch:\n")
+// //fmt.Scan(&str1)
+// fmt.Printf("enter rollno:\n")
+// fmt.Scan(&str, &str1, &rollno)
+// fmt.Printf("Result:%s %s %d\n", str, str1, rollno)
+
+// //var a int = 123
+// var a int = -123
+// var b uint = 0
+// //assign the value of a to the b
+// b = uint(a)
+// //printing the both values
+// fmt.Printf("a=%d,b=%d\n", a, b)
+
+// 	//explicit type function
+// 	var x int = 42
+// 	//converting int to float64
+// 	var y float64 = float64(x)
+// 	//converting float64 to uint
+// 	var z uint = uint(y)
+// 	//printing the types and values of the variables
+// 	fmt.Printf("value of x is %d and type is %T\n", x, x)
+// 	fmt.Printf("value of y is %.2f and type is %T\n", y, y)
+// 	fmt.Printf("Value of z is %d and type is %T\n", z, z)
+
+// }
