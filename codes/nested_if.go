8e519ef95c8060bbab944d46093d372d1533e505
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:17:38 +0530

    Add files via upload

---
+++ codes/nested_if.go
@@ -0,0 +1,36 @@
+package main
+
+//import "fmt"
+
+//nested if
+// func main() {
+// 	x := 8
+// 	y := 10
+
+// 	if x != y {
+// 		if x < y {
+// 			fmt.Println("x is less than y ")
+// 		} else if x > y {
+// 			fmt.Println("x is greater than y")
+// 		}
+// 	} else {
+// 		fmt.Println("x is equal toy")
+// 	}
+// }
+
+// func main() {
+// 	var time float64 = 0.0
+// 	fmt.Printf("enter the time:")
+// 	fmt.Scanf("%f", &time)
+// 	if time <= 23.59 {
+// 		if time <= 10 {
+// 			fmt.Printf("Good Morning")
+// 		} else if time > 10 && time <= 20 {
+// 			fmt.Printf("good afternoon")
+// 		} else {
+// 			fmt.Printf("Good evening")
+// 		}
+// 	} else {
+// 		fmt.Printf("enter the correct time")
+// 	}
+// }
