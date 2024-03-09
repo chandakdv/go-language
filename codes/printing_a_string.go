9d83a9d9bd6a1cd7c6cd6eaf4d711c7a35b3dbec
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:16:02 +0530

    Add files via upload

---
+++ codes/printing_a_string.go
@@ -0,0 +1,18 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// var fname string = "divya"
+// var lname string = "chandak"
+// fmt.Print("my name is", " ", fname, " ", "and my last name is ", " ", lname)
+
+//var i, j = "Hello", " World"
+//fmt.Printf("%s %s", i, j)
+//fmt.Print(j, "\n")
+
+// 	var i, j = 10, "Divya"
+// 	fmt.Print(i, " ", j)
+// }
