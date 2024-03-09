5044e9213ca2469f23fc7a8b14c67245fea5d432
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:15:38 +0530

    Add files via upload

---
+++ codes/recursion.go
@@ -0,0 +1,41 @@
+package main
+
+// import "fmt"
+
+// func countDown(number int) {
+// 	fmt.Println(number)
+// 	countDown(number - 1)
+// }
+// func main() {
+// 	fmt.Println("countdown starts:")
+// 	countDown(3)
+// }
+
+// // recursive function with conditional statement
+// func countDown(number int) {
+// 	if number > 0 {
+// 		fmt.Println(number)
+// 		countDown(number - 1)
+// 	} else {
+// 		fmt.Println("countdown break")
+// 	}
+// }
+// func main() {
+// 	fmt.Println("countdown starts")
+// 	countDown(3)
+// }
+
+// func sum(number int) int {
+// 	if number == 0 {
+// 		return 0
+// 	} else {
+// 		return number + sum(number-1)
+// 	}
+// }
+// func main() {
+// 	var num int
+// 	fmt.Print("Enter number: ")
+// 	fmt.Scanf("%d", &num)
+// 	var result = sum(num)
+// 	fmt.Println("Sum", result)
+// }
