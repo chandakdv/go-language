61ccfe71e814ba3d5a471e6f14e7fe9405f5c875
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 11:39:03 +0530

    Add files via upload

---
+++ codes/even_odd_number.go
@@ -0,0 +1,15 @@
+// number is even or odd
+package main
+
+// import "fmt"
+
+// func main() {
+// 	var num1 int
+// 	fmt.Printf("Enter number 1 ")
+// 	fmt.Scan(&num1)
+// 	if num1%2 == 0 {
+// 		fmt.Println("num1 is even ")
+// 	} else {
+// 		fmt.Println("num1 is odd")
+// 	}
+// }
