4c306cd3479d850057d5db2703c6523d150c2992
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:24:11 +0530

    Add files via upload

---
+++ codes/function.go
@@ -0,0 +1,84 @@
+package main
+
+// //define a functon
+// func green() {
+// 	fmt.Println("Good Morning")
+// }
+// func main() {
+// 	//function call
+// 	green()
+// }
+
+// func addNumbers() {
+// 	n1 := 12
+// 	n2 := 8
+// 	sum := n1 + n2
+// 	fmt.Println("Sum:", sum)
+// }
+// func main() {
+// 	addNumbers()
+// }
+
+// // define a function with parameters
+// func addNumbers(n1 int, n2 int) {
+// 	sum := n1 + n2
+// 	fmt.Println("Sum:", sum)
+// }
+// func main() {
+// 	addNumbers(21, 13)
+// }
+
+// // return value from go function
+// func addNumbers(n1 int, n2 int) int {
+// 	sum := n1 + n2
+// 	return sum
+// }
+// func main() {
+// 	result := addNumbers(21, 13)
+// 	fmt.Println("Sum:", result)
+// }
+
+// func addNumbers(n1 int, n2 int) int {
+// 	sum := n1 + n2
+// 	return sum
+// }
+// func main() {
+// 	result := addNumbers(21, 13)
+// 	fmt.Println("after return statement")
+// 	fmt.Println("Sum:", sum)
+// }
+
+// func calculate(n1 int, n2 int) (int, int) {
+// 	sum := n1 + n2
+// 	difference := n1 - n2
+// 	return sum, difference
+// }
+// func main() {
+// 	sum, difference := calculate(21, 13)
+// 	fmt.Println("Sum:", sum, "difference:", difference)
+// }
+
+//benifits of function
+//1.Code reusability
+
+// func getSquare(num int) {
+// 	square := num * num
+// 	fmt.Println("square of %d is %d\n", num, square)
+// }
+// func main() {
+// 	getSquare(3)
+// 	getSquare(5)
+// 	getSquare(7)
+// }
+
+// // example of local variable
+// func addNumbers() {
+// 	//local variables
+// 	var sum int
+// 	sum = 5 + 9
+// }
+// func main() {
+// 	var sum int = 0
+// 	addNumbers()
+// 	fmt.Println("sum is:", sum)
+// }
