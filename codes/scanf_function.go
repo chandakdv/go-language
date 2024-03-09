f6c39e128ce7df50855c1ced0661f2c379c18a71
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:15:11 +0530

    Add files via upload

---
+++ codes/scanf_function.go
@@ -0,0 +1,25 @@
+package main
+
+//import "fmt"
+// func main() {
+// var name string
+// fmt.Print("Enter your name")
+// fmt.Scanf("%s \n", &name)
+// fmt.Println("Hello", name)
+
+// fmt.Print("Enter first string")
+// var first string
+// fmt.Scanln(&first)
+// fmt.Print("Enter second string")
+// var second string
+// fmt.Scanln(&second)
+// fmt.Println(first + " " + second)
+
+// var a, b int = 10, 20
+// fmt.Println(a, b)
+// fmt.Println(a + b)
+
+// 	var a string = "Divya"
+// 	fmt.printf("Value of a is %s", a)
+
+// }
