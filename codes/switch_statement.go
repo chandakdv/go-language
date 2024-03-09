ef3bed3d46a04160358b0b8b60bc7326be52dc82
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:14:49 +0530

    Add files via upload

---
+++ codes/switch_statement.go
@@ -0,0 +1,74 @@
+package main
+
+//import "fmt"
+
+//switch statements
+// func main() {
+// 	thismonth := 5
+// 	switch thismonth {
+// 	case 1:
+// 		fmt.Println("January")
+// 	case 2:
+// 		fmt.Println("Feb")
+// 	case 3:
+// 		fmt.Println("march")
+// 	case 4:
+// 		fmt.Println("april")
+// 	case 5:
+// 		fmt.Println("may")
+// 	case 6:
+// 		fmt.Println("june")
+// 	}
+// }
+
+// func main() {
+// 	today := time.Now()
+// 	switch today.Day() {
+// 	case 5:
+// 		fmt.Println("today is fifth.clean your house")
+// 	case 10:
+// 		fmt.Println("today is 10th.buy some wine")
+// 	case 15:
+// 		fmt.Println("today is 15th . visit a doctor")
+// 	case 25:
+// 		fmt.Println("today is 15th.Buy some food")
+// 	case 31:
+// 		fmt.Println("today is 31.party tonight")
+// 	default:
+// 		fmt.Println("no info available for the day")
+// 	}
+// }
+
+// func main() {
+// 	x := 1
+
+// 	switch x {
+// 	case 1:
+// 		fmt.Println("1")
+// 		fallthrough
+// 	case 3:
+// 		fmt.Println("3")
+// 		//fallthrough
+// 	case 5:
+// 		fmt.Println("5")
+// 	}
+// }
+
+// func main() {
+// 	//var x interface{}
+// 	var x interface{} = "RKNEC"
+// 	switch i := x.(type) {
+// 	case nil:
+// 		fmt.Printf("type of x:%T", i)
+// 	case int:
+// 		fmt.Printf("x is int")
+// 	case float64:
+// 		fmt.Printf("x is float")
+// 	case func(int) float64:
+// 		fmt.Printf("x is func(int)")
+// 	case bool, string:
+// 		fmt.Printf("x is bool or string")
+// 	default:
+// 		fmt.Printf("don't know the type")
+// 	}
+// }
