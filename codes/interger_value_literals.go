a6e6db728bba0fbe159c65d2931b31c8c4e38cfe
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:23:14 +0530

    Add files via upload

---
+++ codes/interger_value_literals.go
@@ -0,0 +1,21 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// import ("fmt") here no need to import beacuse we don't use it in function
+// func main() {
+// println(15 == 017)
+// println(15 == 0xf)
+
+// var a int = 5
+// var b int = 10
+// a++
+// b--
+// fmt.Printf("%d\n", a, b)
+
+//fmt.Printf(15 == 0xf)
+// 	fmt.Printf("expression is == oxf is %t \n", 15 == 0xf) //expression is == oxf is true
+// 	fmt.Printf("expression is == oxf is %T", 15 == 0xf)    // OUTPUT =expression is == oxf is bool
+// }
