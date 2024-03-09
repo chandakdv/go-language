0c10749f1015cf5147c3daa5d8ab5ade0c552245
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:14:28 +0530

    Add files via upload

---
+++ codes/two_libraries_input.go
@@ -0,0 +1,59 @@
+package main
+
+//import "fmt"
+// 	"math"
+
+// func main() {
+// var val float64 = -19.25
+// fmt.Printf("Absolute value of %f is %f", val, math.Abs(val))
+
+//finding maximum number
+// var num1 float64 = 10.25
+// var num2 float64 = 20.14
+// var large float64 = 0
+// //large = math.Max(num1, num2)
+// large = math.Min(num1, num2)
+// fmt.Printf("largest number is :%f", large)
+
+//ceil function
+// var result float64 = 0
+// result = math.Ceil(num1)
+// fmt.Printf("Result is :%f", result)
+// result = math.Ceil(num2)
+// fmt.Printf("\nResult is:%f", result)
+
+//power function
+// var num = 10.0
+// var p = 2.0
+// var result float64
+// result = math.Pow(num, p)
+// fmt.Printf("Power is %f", result)
+
+// var i = 3.141
+// fmt.Printf("%e\n", i)
+// fmt.Printf("%f\n", i)
+// fmt.Printf("%.2f\n", i)
+// fmt.Printf("%6.2f\n", i)
+// fmt.Printf("%g\n", i)
+
+//printing without package
+//println("using println ionstead of fmt.println")
+//print("using print onstead of fmt.print")
+
+//printing without package
+// print("using println ionstead of fmt.println")
+// print("using print onstead of fmt.print")
+
+// x := 10
+// x++
+// fmt.Println(x)
+
+// var x = 10
+// x += 0
+// fmt.Println(x)
+
+// var number int
+// fmt.Print("Enter the number")
+// fmt.Scanf("%d\n", &number)
+
+// }
