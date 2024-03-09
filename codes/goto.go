3a6303dd9804cd48c988b1e137fd32c96f544bea
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:24:33 +0530

    Add files via upload

---
+++ codes/goto.go
@@ -0,0 +1,38 @@
+package main
+
+// import (
+// 	"fmt "
+// 	"strings"
+// )
+
+// func main() {
+// 	i := 0
+// loop:
+// 	fmt.Println(i)
+// 	i++
+// 	if i < 5 {
+// 		goto loop
+// 	}
+// 	fmt.Println("loop ends here")
+// }
+
+// func main() {
+// 	var str string = "hellow world"
+// 	var subStr string = "hor"
+// 	if strings.Contains(str, subStr) == true {
+// 		fmt.Printf("String (%s) contains substring (%S)", str, subStr)
+// 	} else {
+// 		fmt.Printf("String (%s) does not conatins substring(%s)", str, subStr)
+// 	}
+// }
+
+// func main() {
+// 	var str string = "Hello World"
+// 	var ind int = 0
+// 	ind = strings.Index(str, "W")
+// 	fmt.Println("index is :", "w")
+// 	var result string
+// 	result = strings.ToLower(str)
+// 	//result = string.Toupper(str)
+// 	fmt.Println("string is lower case :", result)
+// }
