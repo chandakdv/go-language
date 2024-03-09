fe0dea72f2ce6ad93b260a76951d9ead5ec59471
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 11:45:13 +0530

    Add files via upload

---
+++ codes/area_of_square.go
@@ -0,0 +1,26 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	const (
+// 		LENGTH int = 10
+// 		WIDTH  int = 5
+// 		PI         = 3.14
+// 		R          = 5
+// 		A          = 10
+// 	)
+// 	area_sq := A * A
+// 	fmt.Printf("Area of Square : %d\n", area_sq)
+// 	area_rect := LENGTH * WIDTH
+// 	fmt.Printf("Area of Rectangle : %d\n", area_rect)
+// 	var area_cir float64
+// 	area_cir = PI * R * R
+// 	fmt.Printf("Area of Circle : %f\n", area_cir)
+// }
+
+/*Keywords in go
+break deafult func interface select case defer GO map
+Struct chan else Goto package Switch const fallthrough
+if range type continue for import return var
+*/
