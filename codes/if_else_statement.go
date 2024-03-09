5040bbed81c37f260cd738cc96d8800aaa78f3d7
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:23:31 +0530

    Add files via upload

---
+++ codes/if_else_statement.go
@@ -0,0 +1,67 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	x := 10
+// 	y := 20
+// 	if x > y {
+// 		fmt.Println("x is less than y")
+// 	} else {
+// 		fmt.Println("x is greater than y")
+// 	}
+// }
+
+//printing student marksheet using if else statement
+// func main() {
+// 	var marks1 int
+// 	var marks2 int
+// 	var marks3 int
+// 	var name string
+// 	var roll int
+// 	var m int
+// 	fmt.Printf("Enter Name ")
+// 	fmt.Scan(&name)
+// 	fmt.Printf("Enter Roll number ")
+// 	fmt.Scan(&roll)
+// 	fmt.Printf("Enter marks 1 ")
+// 	fmt.Scan(&marks1)
+// 	fmt.Printf("Enter marks 2 ")
+// 	fmt.Scan(&marks2)
+// 	fmt.Printf("Enter marks 3 ")
+// 	fmt.Scan(&marks3)
+// 	m = (((marks1 + marks2 + marks3) * 100) / 300)
+// 	fmt.Println("Percentage=", m)
+// 	if m > 40 {
+// 		fmt.Println("Student is passed ")
+// 	} else {
+// 		fmt.Println("Student is failed")
+// 	}
+// }
+
+// there are three type of functions in go langugae
+// the print() function prints its argument with their default format
+
+//another type of printing the marksheet
+// func main() {
+// 	var name1, name2, name3, name4 string = "Divya ", "archie", "khushi", "vaidehi"
+// 	var paper21, paper22, paper23, paper24 int = 13, 12, 14, 12
+// 	var rollno1, rollno2, rollno3, rollno4 int = 05, 03, 23, 24
+// 	var paper11, paper12, paper13, paper14 int = 13, 12, 11, 10
+// 	fmt.Println("my name is :", name1)
+// 	fmt.Println("my name is :", name2)
+// 	fmt.Println("my name is :", name3)
+// 	fmt.Println("my name is :", name4)
+// 	fmt.Println("my rollno is:", rollno1)
+// 	fmt.Println("my rollno is:", rollno2)
+// 	fmt.Println("my rollno is:", rollno3)
+// 	fmt.Println("my rollno is:", rollno4)
+// 	fmt.Println("my paper 1 marks are  :", paper11)
+// 	fmt.Println("my paper 1 marks are  :", paper12)
+// 	fmt.Println("my paper 1 marks are  :", paper13)
+// 	fmt.Println("my paper 1 marks are  :", paper14)
+// 	fmt.Println("my paper 2 marks are  :", paper21)
+// 	fmt.Println("my paper 2 marks are  :", paper22)
+// 	fmt.Println("my paper 2 marks are  :", paper23)
+// 	fmt.Println("my paper 2 marks are  :", paper24)
+// }
