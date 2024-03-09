df570c9c0230629199acdcce58c7e7536bd29075
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 11:38:17 +0530

    Add files via upload

---
+++ codes/fibonacci_using_recurssion.go
@@ -0,0 +1,35 @@
+package main
+
+// import "fmt"
+
+// // fibonacci series
+// func fibonacci(num int) {
+// 	var a, b int
+// 	for a, b = 0, 1; a <= num; a, b = b, a+b {
+// 		fmt.Printf("%d\t", a)
+// 	}
+// 	fmt.Println()
+// }
+// func main() {
+// 	var num int
+// 	fmt.Print("Enter number: ")
+// 	fmt.Scanf("%d", &num)
+// 	fibonacci(num)
+// }
+
+// func fibonacci(n int) int {
+// 	if n <= 1 {
+// 		return n
+// 	}
+// 	return fibonacci(n-1) + fibonacci(n-2)
+// }
+// func main() {
+// 	var n int
+// 	fmt.Print("Enter the number  ")
+// 	fmt.Scan(&n)
+
+// 	fmt.Println("Fibonacci Series:")
+// 	for i := 0; i < n; i++ {
+// 		fmt.Printf("%d ", fibonacci(i))
+// 	}
+// }
