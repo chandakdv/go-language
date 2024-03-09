129710c95a149619ba92e7d2f66c46c478b9708a
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 11:44:04 +0530

    Add files via upload

---
+++ codes/arithematic_operations.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	a := 10
+// 	b := 20
+// 	sum := a + b
+// 	sub := a - b
+// 	mul := a * b
+// 	div := a / b
+// 	fmt.Println("Sum of two numbers is", sum)
+// 	fmt.Println("Substraction of two numbers is", sub)
+// 	fmt.Println("multiplication of two numbers is", mul)
+// 	fmt.Println("division of two numbers is", div)
+// }
+
+// func main() {
+// 	var num1 int
+// 	var num2 int
+// 	var avg float64
+// 	num1 = 10
+// 	num2 = 5
+// 	avg = (float64(num1) + float64(num2)) / 2
+// 	//avg=(float32(num1)+float32(num2))/2 ##cannot use (float32(num1) + float32(num2)) / 2 (value of type float32) as float64 value in assignment
+// 	//avg=((num1)+(num2))/2 ##cannot use ((num1) + (num2)) / 2 (value of type int) as float64 value in assignment
+// 	fmt.Printf("Average of %d and %d is %.2f\n", num1, num2, avg)
+// }
