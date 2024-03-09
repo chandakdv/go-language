37e2ef41a0b04c865b907afd4949620f2bf8aa03
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-09 09:24:55 +0530

    Add files via upload

---
+++ codes/format_specifier.go
@@ -0,0 +1,30 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// var i = 15.5
+// var txt = " hello world !"
+// fmt.Printf("%v\n", i) //%v =print in actual type
+// fmt.Printf("%#v\n", i)
+// fmt.Printf("%v%%\n", i) // ek baar % print karna hai to 2 baar % likhna padega
+// fmt.Printf("%T\n", i)
+// fmt.Printf("%v\n", txt)
+// fmt.Printf("%#v\n", txt)
+// fmt.Printf("%T\n", txt) //%T = print karta hai type
+
+// 	var i = 15
+// 	fmt.Printf("%b\n", i)
+// 	fmt.Printf("%d\n", i)
+// 	fmt.Printf("%+d\n", i)
+// 	fmt.Printf("%o\n", i)
+// 	fmt.Printf("%O\n", i)
+// 	fmt.Printf("%x\n", i)
+// 	fmt.Printf("%X\n", i)
+// 	fmt.Printf("%#x\n", i)
+// 	fmt.Printf("%4d\n", i)
+// 	fmt.Printf("%-4d\n", i)
+// 	fmt.Printf("%04d\n", i)
+// }
