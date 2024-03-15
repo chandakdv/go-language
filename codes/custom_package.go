fcd8ed2a4e43eec3939a7e4b326cd9617b3de55d
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-15 14:52:24 +0530

    Add files via upload

---
+++ codes/custom_package.go
@@ -0,0 +1,54 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// // example of do...while loop
+// func main() {
+// 	number := 1
+// 	//loop that runs infinety
+// 	for {
+// 		//condition to terminate the loop
+// 		fmt.Printf("%d\n", number)
+// 		number++
+// 		if number > 5 {
+// 			break
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	multiplier := 1
+// 	//run while loop for 10 times
+// 	for multiplier <= 10 {
+// 		//find the product
+// 		product := 5 * multiplier
+// 		//print the multiplication table in format 5*1 = 5
+// 		fmt.Printf("5*%d=%d\n", multiplier, product)
+// 		multiplier++
+// 	}
+// }
+
+// func main() {
+// 	colors := []string{"Red", "Yellow", "Green"}
+// 	// for index, val := range colors {
+// 	// 	fmt.Println(index, "-", val)
+// 	// }
+// 	for _, val := range colors {
+// 		fmt.Println(val)
+// 	}
+// }
+
+// func main() {
+// 	var arrayOfInteger = [5]int{1, 5, 8, 0, 3}
+// 	fmt.Println(arrayOfInteger)
+// 	fmt.Printf("Type : %T", arrayOfInteger)
+// }
+
+// func main() {
+// 	//declare array variable of type string
+// 	//undefined size
+// 	var arrayOfString = [...]string{"Hello", "Rajesh"}
+// 	fmt.Println(arrayOfString)
+// }
