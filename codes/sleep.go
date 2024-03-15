0c9fcb1e6af1bdb252a5fd3540686e372b2db80f
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-15 14:46:11 +0530

    Add files via upload

---
+++ codes/sleep.go
@@ -0,0 +1,10 @@
+package main
+
+//import("fmt")
+// func main() {
+// 	fmt.Println("Hello World")
+// 	time.Sleep(5000 * time.Millisecond)
+// 	fmt.Println("Hello India")
+// 	time.Sleep(8 * time.Second)
+// 	fmt.Println("Hello , welcome to golang")
+// }
