f56a994bc4a0536847d2833367c1c73ebf9be2d2
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-15 14:56:08 +0530

    Add files via upload

---
+++ codes/sorting.go
@@ -0,0 +1,30 @@
+package main
+
+//import "fmt"
+
+// // sorting an array
+// func main() {
+// 	//Define an array of inetegers
+// 	numbers := [6]int{4, 2, 7, 1, 9, 3}
+// 	//use sort.Ints() tp sort the array
+// 	sort.Ints(numbers)
+// 	//Print the sorted array
+// 	fmt.Println("Sorted", numbers)
+// }
+
+// // sorting one dimensional float array
+// func main() {
+// 	//define an array float
+// 	numbers := []float64{4.2, 2.7, 7.1, 1.5, 9.0, 3.3}
+// 	//use sort.Float64() to sort the array
+// 	sort.Float64s(numbers)
+// 	//print the sorted array
+// 	fmt.Println("sorted:", numbers)
+// }
+
+// // sorting single dimentional string array using golang
+// func main() {
+// 	words := []string{"banana", "apple", "orange", "grapes", "pineapple"}
+// 	sort.Strings(words)
+// 	fmt.Println("sorted:", words)
+// }
