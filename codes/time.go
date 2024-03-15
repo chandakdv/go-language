758cb54c132c4528d71a093d6f29e1c0d511af1c
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2024-03-15 14:44:48 +0530

    Add files via upload

---
+++ codes/time.go
@@ -0,0 +1,30 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	currentDateTime := time.Now()
+// 	day := currentDateTime.Day()
+// 	month := currentDateTime.Month()
+// 	year := currentDateTime.Year()
+// 	hour := currentDateTime.Hour()
+// 	min := currentDateTime.Minute()
+// 	sec := currentDateTime.Second()
+// 	fmt.Printf("\nDay:%d", day)
+// 	fmt.Printf("\nMonth:%d", month)
+// 	fmt.Printf("\nYear:%d", year)
+// 	fmt.Printf("\nHour:%d", hour)
+// 	fmt.Printf("\nMinute:%d", min)
+// 	fmt.Printf("\nSecond:%d", sec)
+// }
+
+// func main() {
+// 	YYYY, MM, DD := time.Now().Date()
+// 	// fmt.Println("Date:", DD)
+// 	// fmt.Println("Month:", MM)
+// 	// fmt.Println("Year:", YYYY)
+// 	fmt.Println(DD, MM, YYYY)
+// }
